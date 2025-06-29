73b798390b0118b3a11377bfee731f84c30d5907
Martin Emde <me@martinemde.com>
2025-06-29 12:56:00 -0700

    chore: remove old typescript implementation

--- src/cli.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { Studio } from './studio';
-
-// Get command line arguments (excluding node and script path)
-const argv = process.argv.slice(2);
-
-// Start the server
-Studio.serve(argv);
--- test/setup.ts
+++
@@ -1,5 +0,0 @@
-// Test setup file for Jest
-// Add any global test configuration here
-
-// Increase default timeout for integration tests
-jest.setTimeout(10000);
